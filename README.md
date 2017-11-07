61599f54e705cb5c95ee036bceaea6cd1190687e
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2017-11-07 09:53:23 -0600

    chore(package): Update test files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # patternlab-node-3.0-demo-vanilla
-Minimal example of what you need for Pattern Lab 3.0
+Minimal example of what you need for [Pattern Lab 3.0](https://github.com/pattern-lab/patternlab-node)
+
+* `index.js` contains a simple example of interacting with Pattern Lab Node and its events
+
+Files from starterkit-mustache-demo included  directly for demonstration purposes only.
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const config = require('./patternlab-config.json');
 const patternlab = require('@pattern-lab/patternlab-node')(config);
 
-patternlab.serve({
+// console.log(`Pattern Lab v${patternlab.v()}`);
+
+patternlab.serve(() => {
+  // use the callback
+}, {
   cleanPublic: true,
-  watch: true
+}).then(() => {
+  // or do something else when this promise resolves
 });
 
+//optionally, listen on events
 // patternlab.events.on('patternlab-asset-change', (data) => {
 //   console.log(data); // {file: 'path/to/file.css', dest: 'path/to/destination'}
 // });
